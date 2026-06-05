468dedfef51479aca534b39b5a2e3895818dafe3
tensorbytes0202 <aditya11501246@gmail.com>
2026-06-05 22:06:59 +0530

    update commit

--- Pydantic/pydantic_why.py
+++ Pydantic/pydantic_why.py
@@ -1,17 +1,30 @@
-from pydantic import BaseModel, EmailStr, AnyUrl
-from typing import List, Dict, Optional
+from multiprocessing import Value
+from pydantic import BaseModel, EmailStr, AnyUrl,Field,field_validator
+from typing import List, Dict, Optional,Annotated
 
 
 class Patient(BaseModel):
-    name: str
+    name: Annotated[str,Field(max_length=50,title='Name of the patient',
+    description='Give the name of the patient in less than 50 chars',examples=['Nitish','Amit'])]
     email: EmailStr
     linkedin_url: AnyUrl
-    age: int
-    weight: float
-    married: bool
-    allergies: Optional[List[str]] = None
+    age: int = Field(gt=0,lt=120)
+    weight: Annotated[float,Field(gt=0,strict=True)]
+    married: Annotated[bool,Field(default=None,description='Is the patient married or not')]
+    allergies: Annotated[Optional[List[str]],Field(default=None,max_length=5)]
     contact_details: Dict[str, str]
 
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):
+
+        valid_domains = ['hdfc.com','icici.com']
+        # abc@gmail.com
+
+        domain_name = value.split('@')[-1]
+
+        if domain_name not in valid_domains:
+            raise ValueError('Not a valid domain')
 
 def insert_patient_data(patient: Patient):
     print(patient.name)
@@ -29,7 +42,7 @@ def update_patient_data(patient: Patient):
 
 patient_info = {
     "name": "nitish",
-    "email": "aditya@gmail.com",
+    "email": "aditya@hdfc.com",
     "linkedin_url": "https://linkedin.com",  # Fixed
     "age": 30,
     "weight": 55.4,
