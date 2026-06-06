b752ead695a0e0475c025abc1b876333164e5df3
tensorbytes0202 <aditya11501246@gmail.com>
2026-06-06 23:27:44 +0530

    new commit

--- Post_request.py
+++ Post_request.py
@@ -1,21 +1,20 @@
-from IPython import core
-from IPython import core
-from dns import grange
-from functools import _Descriptor
+
+from dns.rdtypes.IN import HTTPS
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI,Path,HTTPException,Query
-from pydantic import BaseModel,Feild,computed_field
+from pydantic import BaseModel,Field,computed_field
 from typing import Annotated,Literal
 import json
 app= FastAPI()
 
 class Patient(BaseModel):
-    id:Annotated[str,Feild(..., description='ID of the patient',examples=['P001'])]
-    name:Annotated[str,Feild(..., description='Name of the patient')]
-    city:Annotated[str,Feild(...,description='city where the patient is liying')]
-    age:Annotated[int,Feild(...,gt=0,lt=120,decription='Age of the patient')]
-    gender: Annotated[Literal['male','Female','Other'],Feild(..., decrition='Gender of the patient')]
-    height:Annotated[float,Feild(...,gt=0,description='Height of the patient in mtrs')]
-    weight: Annotated[float,Feild(...,gt=0,description='Weight of the patient in kgs')]
+    id:Annotated[str,Field(..., description='ID of the patient',examples=['P001'])]
+    name:Annotated[str,Field(..., description='Name of the patient')]
+    city:Annotated[str,Field(...,description='city where the patient is liying')]
+    age:Annotated[int,Field(...,gt=0,lt=120,decription='Age of the patient')]
+    gender: Annotated[Literal['male','Female','Other'],Field(..., decrition='Gender of the patient')]
+    height:Annotated[float,Field(...,gt=0,description='Height of the patient in mtrs')]
+    weight: Annotated[float,Field(...,gt=0,description='Weight of the patient in kgs')]
     
     @computed_field
     @property
@@ -37,5 +36,72 @@ class Patient(BaseModel):
 def load_data():
     with open('patients.json','r') as f:
         data = json.load(f)
+    
+    return data
+
+def save_data(data):
+    with open('patients.json','w') as f:
+        json.dump(data,f)
+
+
+@app.get("/")
+
+def hello():
+    return {'message':'Hello world'}
+
+@app.get('/about')
+def about():
+    return {'message':'hello its adi'}
+#endpoints
+@app.get('/view')
+def view():
+    data = load_data()
 
     return data
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str = Path(..., description='ID of the patient in the DB', example='P001',)):
+
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404,detail='Patient not found')
+
+
+@app.get('/sort')
+def sort_patient(sort_by: str= Query(...,description='Sort on the basis of hieght,weight or bmi'),
+order: str = Query('asc', description='sort in asc or dsc order')):
+
+    valid_fields = ['height','weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f'Invalid field select from {valid_fields}')
+    
+
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail='Invalid order select between asc and desc')
+
+    data = load_data()
+    sort_order = True if order == 'desc' else False
+
+    sorted_data = sorted(data.values(),key=lambda x: x.get(sort_by,0),reverse=sort_order)
+    return sorted_data
+
+@app.post('/create')
+def create_patient(patient:Patient):
+
+   #load existing data
+   data = load_data()
+
+   #check if the patient already exists
+   if patient.id in data:
+    raise HTTPException(status_code=400,detail='Patiet already exists')
+
+   #new parient add to the databases
+
+   data[patient.id] =patient.model_dump(exclude=['id'])
+
+   #save into the json file
+   save_data(data)
+
+   return JSONResponse(status_code=201,content={'message':'patient created successfully'})
